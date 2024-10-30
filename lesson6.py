f5d3d24281f80bb3fe29cd8f750e568779501f97
AndryDiv <andry.divanovski@gmail.com>
2024-10-30 23:52:13 +0300

    lesson6

---
+++ lesson6.py
@@ -0,0 +1,15 @@
+name = "Andry"
+syrname = 'Divanovski'
+print(name, syrname)
+
+fullname = name + ' ' + syrname
+print(fullname)
+code = 'o'*5
+print(code)
+
+chislo1 = 405
+str1 = str(chislo1)
+print(type(str1))
+chislo2 = 3.45
+str2 = str(chislo2)
+print(type(str2))
